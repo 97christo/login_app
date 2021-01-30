36d04436fe83fdde4eaa44894373d3b2d7b13f13
kychung <97christo@gmail.com>
2021-01-30 18:16:57 +0900

    db information changed to private DB, also added some error print to make debugging easier

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -23,61 +23,62 @@ public class LoginServlet extends GenericServlet {
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		String input_id = request.getParameter("id");
 		String input_pw = request.getParameter("pw");
-		
+
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 
-		String db_url = "jdbc:mysql://test-db1.cjq318fitl2e.ap-northeast-2.rds.amazonaws.com:3306";
+		String db_url = "jdbc:mysql://db-master.cjq318fitl2e.ap-northeast-2.rds.amazonaws.com:3306";
 		String db_user = "admin";
 		String db_pwd = "qwer1234";
-		String db_name = "/database1";
+		String db_name = "/testDB";
+		String tbl_name = "test_tbl";
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			out.println(e.getMessage());
 		}
 
 		try {
 			Connection conn = DriverManager.getConnection(db_url + db_name, db_user, db_pwd);
 
-			String query = "SELECT * FROM userData WHERE id='" + input_id + "'";
+			String query = "SELECT * FROM " + tbl_name + " WHERE id='" + input_id + "'";
 			System.out.println("query: " + query);
-			
+
 			PreparedStatement ps = conn.prepareStatement(query);
 			ResultSet rs = ps.executeQuery();
-			
+
 			boolean isEmpty = true;
-			
-			if(rs.next()) {
+
+
+			if (rs.next()) {
 				isEmpty = false;
-				
+
 				String result_id = rs.getString("id");
 				String result_pw = rs.getString("pw");
-				
-				if(result_pw.equals(input_pw)) {
+
+				if (result_pw.equals(input_pw)) {
 					out.println("<p>로그인 성공! (id: " + result_id + ")</p>");
 				} else {
 					out.println("<p>로그인 실패. (pw 틀림)</p>");
-				}				
+				}
 			}
-			
-			if(isEmpty) {
+
+			if (isEmpty) {
 				out.println("<p>존재하지 않는 id</p>");
 			}
-			
+
 			rs.close();
 			ps.close();
-			conn.close();			
-			
+			conn.close();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
+			out.println(e.getMessage());
 			e.printStackTrace();
 		}
-
-
 
 	}
 
